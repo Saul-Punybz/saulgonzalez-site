a140ddc7d54983e9a7ad81490a3eb2347fdd1e7c
Saul Gonzalez <saulgonzalez@Sauls-MacBook-Pro.local>
2026-04-23 13:43:50 -0400

    CSP: allow clarity.ms for analytics

--- next.config.js
+++ next.config.js
@@ -9,11 +9,11 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.clarity.ms",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https:",
-      "connect-src 'self'",
+      "connect-src 'self' https://www.clarity.ms https://*.clarity.ms",
       "frame-ancestors 'none'",
     ].join('; '),
   },

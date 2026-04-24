aa953640c8e1454b366360bfb3fb19b9fd012b02
Saul Gonzalez <saulgonzalez@Sauls-MBP.lan>
2026-04-23 22:16:14 -0400

    Fix Clarity CSP: allow scripts.clarity.ms + z.clarity.ms + remove ref=bwt

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const securityHeaders = [
     key: 'Content-Security-Policy',
     value: [
       "default-src 'self'",
-      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.clarity.ms https://www.googletagmanager.com https://www.google-analytics.com",
+      "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.clarity.ms https://www.clarity.ms https://www.googletagmanager.com https://www.google-analytics.com",
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "font-src 'self' https://fonts.gstatic.com",
       "img-src 'self' data: blob: https:",
